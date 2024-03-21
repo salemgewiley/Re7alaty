0dcc45011859350b78ee6c583a1dad7d9da22b33
Salem Gewiley <105030635+salemgewiley@users.noreply.github.com>
2024-03-22 01:05:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ document.querySelectorAll(".item-link").forEach((item) => {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  var planCards = document.querySelectorAll('.plan-card');
+
+  planCards.forEach(function (card) {
+    card.addEventListener('click', function () {
+      planCards.forEach(function (sibling) {
+        sibling.classList.remove('cardActive');
+      });
+      this.classList.add('cardActive');
+    });
+  });
+});
+
+
